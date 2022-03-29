717dd65f1ffec7b3b8c069b1afca241705260368
Arthur Holanda <holandaarthur46@gmail.com>
2022-03-29 11:17:59 -0300

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
 
 
 from app.controllers import default 
