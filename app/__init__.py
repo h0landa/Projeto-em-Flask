f62692f0a3dd3e09af1822fe37b439d42856eb00
Arthur Holanda <holandaarthur46@gmail.com>
2022-03-30 15:41:09 -0300

    Criação dos MIGRATIONS

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dados.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
