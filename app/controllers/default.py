979f1cee747ad0d961f8cb5248c9a7a13ef96b82
Arthur Holanda <holandaarthur46@gmail.com>
2022-04-08 15:26:16 -0300

    Salvando

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -14,22 +14,25 @@ def hello():
 @app.route('/form/', methods=['POST', 'GET'])
 def formulario():
     form = MyForm()
+    mensagem = ''
+    login = ''    
     if request.method == 'POST':
         cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         user = form.username.data
         password = form.password.data
         email = form.email.data
         date = form.date.data
-        cur.execute(f"SELEC * FROM login WHERE username = {user}")
-        login = cur.fetchone()
+        cur.execute(f"SELECT * FROM login WHERE username = '{user}';")
+        login = cur.fetchall()
         if login:
             mensagem = 'Esse usuário já existe, tente novamente'
         else:
             cur.execute(
                 f"INSERT INTO login(username, password, email, date) VALUES('{user}', '{password}', '{email}', '{date}');")
             mysql.connection.commit()
+            mensagem = 'Usuário cadastrado com sucesso'
     return render_template('form_page.html',
-                           form=form)
+                           form=form, mensagem = mensagem, login = login)
 
 
 @app.route('/hello', defaults={'name': None})
