ca4aaeedf89746b52d3c0ed1ff1b15042883a151
Arthur Holanda <holandaarthur46@gmail.com>
2022-04-12 13:53:07 -0300

    Criando o método lm.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 import os
+from flask_login import LoginManager
 
 
 file_path = os.path.abspath(os.getcwd())+"\dados.db"
@@ -12,6 +13,6 @@ migrate = Migrate(app, db)
 app.config.from_object('config')
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
-
+lm = LoginManager(app)
 
 from app.controllers import default 
