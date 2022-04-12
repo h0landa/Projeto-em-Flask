503540a24a3845f10f5c0a985da4b829b580b6f0
Arthur Holanda <holandaarthur46@gmail.com>
2022-04-12 17:02:08 -0300

    Organizando o código.

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -9,8 +9,8 @@ from flask_login import login_user
 def index():
     return render_template('index.html')
 
-    
-@app.route('/login/', methods=['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     Loginform = LoginForm()
     mensagem = ''
@@ -22,26 +22,28 @@ def login():
             return redirect(url_for('index'))
         else:
             mensagem = 'Usuário/Senha Incorretos. Tente novamente'
-    return render_template('login_page.html', 
-                            Loginform = Loginform, msg = mensagem)
+    return render_template('login_page.html',
+                           Loginform=Loginform, msg=mensagem)
 
 
 @app.route('/form/', methods=['POST', 'GET'])
 def formulario():
     Registerform = RegisterForm()
     mensagem = ''
-    login = ''    
+    login = ''
     if request.method == 'POST':
-        login = User.query.filter_by(username=Registerform.username.data).first()
-        if login: 
+        login = User.query.filter_by(
+            username=Registerform.username.data).first()
+        if login:
             mensagem = 'Esse usuário já existe, tente novamente'
         else:
-            new_user = User(f'{Registerform.username.data}', f'{Registerform.password.data}', f'{Registerform.email.data}', f'{Registerform.date.data}')        
+            new_user = User(f'{Registerform.username.data}', f'{Registerform.password.data}',
+                            f'{Registerform.email.data}', f'{Registerform.date.data}')
             db.session.add(new_user)
             db.session.commit()
             mensagem = 'Usuário cadastrado com sucesso'
     return render_template('form_page.html',
-                           Registerform = Registerform, mensagem = mensagem)
+                           Registerform=Registerform, mensagem=mensagem)
 
 
 @app.route('/hello', defaults={'name': None})
@@ -51,11 +53,3 @@ def hello_word(name):
         return f'Olá, %s!' % name
     else:
         return f'Olá,usuário!'
-
-
-'''@app.route('/usuarios')
-def users():
-    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cur.execute('SELECT * FROM login;')
-    result = cur.fetchall()
-    return str(result)'''
